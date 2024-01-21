fdbf2ed177d4706c6552c25ead129619b0788d95
Amir <amir.gh.tehrani@gmail.com>
2024-01-20 21:38:00 -0600

    commit8

--- test.py
+++ test.py
@@ -5,13 +5,14 @@ from airplane_simulation.first_airplane import airplane_lrh
 
 Air = airplane_lrh()
 
-AA = np.arange(0, 20, 1)
+X = np.arange(0, 20, 1)
 
-y = 2 * AA + 5 
+y = 2 * X + 5 
 plt.title("Matplotlib demo") 
 plt.xlabel("x axis caption") 
 plt.ylabel("y axis caption") 
-plt.plot(AA, y) 
+plt.plot(X, y) 
 plt.show()
 
 print("Hello class")
+plt.savefig("my_plot.png")  # Save the plot to a file
